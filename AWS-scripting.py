9233c74e7a2fe2c0908a8fab377a3c8344e16a73
Lalinikant <lalinikant.c@gmail.com>
2023-08-07 20:25:18 +0530

    done

---
+++ AWS-scripting.py
@@ -0,0 +1,10 @@
+import tkinter
+
+from tkinter import Tk
+from tkinter import ttk
+from tkinter import *
+
+root=Tk()
+root.title("First Python GUI")
+ttk.Button(root, text="Hello Lalinikant!!").grid()
+root.mainloop()
